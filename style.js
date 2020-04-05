5fd078c0a23dcd71854506811cc1a553b1b927e8
Ryan Siverson <rcsskier@mac.com>
2020-04-05 12:45:42 -0600

    clears results before writing results o the page

--- style.js
+++ style.js
@@ -22,8 +22,8 @@ $("#submit-btn").on("click", function (searchCity) {
 
         var kFL = (response.main.feels_like - 273.15) * 1.80 + 32;
         var k = (response.main.temp - 273.15) * 1.80 + 32;
-        var cityTemp = $('<h6>').text("Temperature: " + k.toFixed(1) + " F");
-        var cityFL = $('<h6>').text("Feels Like: " + kFL.toFixed(1) + " F");
+        var cityTemp = $('<h6>').text("Temperature: " + k.toFixed(1) + " ºF");
+        var cityFL = $('<h6>').text("Feels Like: " + kFL.toFixed(1) + " ºF");
         var cityHum = $('<h6>').text("Humidity: " + response.main.humidity + "%");
         var cityWind = $('<h6>').text("Wind Speed: " + response.wind.speed + " mph");
         var wURL = "<img src='http://openweathermap.org/img/wn/";
@@ -31,6 +31,8 @@ $("#submit-btn").on("click", function (searchCity) {
         var wCI = response.weather[0].icon;
         var wCIcon = (wURL + wCI + wURL2);
         var topWeather = (response.name + '     (' + moment().format('l') + ')    ' + wCIcon);
+        $("#cityNameBanner").empty();
+        $("#cityWeather").empty();
         $("#cityNameBanner").append(topWeather);
         $("#cityWeather").append(cityTemp, cityFL, cityHum, cityWind);
 
@@ -62,17 +64,17 @@ $("#submit-btn").on("click", function (searchCity) {
         var k5 = (data.list[39].main.temp_max - 273.15) * 1.80 + 32;
 
 
-        var cityTemp1 = $('<h6>').text("Temp: " + k1.toFixed(2) + " F");
-        var cityTemp2 = $('<h6>').text("Temp: " + k2.toFixed(2) + " F");
-        var cityTemp3 = $('<h6>').text("Temp: " + k3.toFixed(2) + " F");
-        var cityTemp4 = $('<h6>').text("Temp: " + k4.toFixed(2) + " F");
-        var cityTemp5 = $('<h6>').text("Temp: " + k5.toFixed(2) + " F");
+        var cityTemp1 = $('<h6>').text(" Temp: " + k1.toFixed(2) + " ºF");
+        var cityTemp2 = $('<h6>').text(" Temp: " + k2.toFixed(2) + " ºF");
+        var cityTemp3 = $('<h6>').text(" Temp: " + k3.toFixed(2) + " ºF");
+        var cityTemp4 = $('<h6>').text(" Temp: " + k4.toFixed(2) + " ºF");
+        var cityTemp5 = $('<h6>').text(" Temp: " + k5.toFixed(2) + " ºF");
 
-        var cityHum1 = $('<h6>').text("Humidity: " + data.list[7].main.humidity + "%");
-        var cityHum2 = $('<h6>').text("Humidity: " + data.list[15].main.humidity + "%");
-        var cityHum3 = $('<h6>').text("Humidity: " + data.list[23].main.humidity + "%");
-        var cityHum4 = $('<h6>').text("Humidity: " + data.list[31].main.humidity + "%");
-        var cityHum5 = $('<h6>').text("Humidity: " + data.list[39].main.humidity + "%");
+        var cityHum1 = $('<h6>').text(" Humidity: " + data.list[7].main.humidity + "%");
+        var cityHum2 = $('<h6>').text(" Humidity: " + data.list[15].main.humidity + "%");
+        var cityHum3 = $('<h6>').text(" Humidity: " + data.list[23].main.humidity + "%");
+        var cityHum4 = $('<h6>').text(" Humidity: " + data.list[31].main.humidity + "%");
+        var cityHum5 = $('<h6>').text(" Humidity: " + data.list[39].main.humidity + "%");
 
         var wI1 = data.list[7].weather[0].icon;
         var wI2 = data.list[15].weather[0].icon;
@@ -88,7 +90,11 @@ $("#submit-btn").on("click", function (searchCity) {
         var wIcon4 = (wURL + wI4 + wURL2);
         var wIcon5 = (wURL + wI5 + wURL2);
 
-
+        $("#fiveDay1Date").empty();
+        $("#fiveDay2Date").empty();
+        $("#fiveDay3Date").empty();
+        $("#fiveDay4Date").empty();
+        $("#fiveDay5Date").empty();
         $("#fiveDay1Date").append(moment().add(1, 'days').format('l'), cityTemp1, wIcon1, cityHum1);
         $("#fiveDay2Date").append(moment().add(2, 'days').format('l'), cityTemp2, wIcon2, cityHum2);
         $("#fiveDay3Date").append(moment().add(3, 'days').format('l'), cityTemp3, wIcon3, cityHum3);
